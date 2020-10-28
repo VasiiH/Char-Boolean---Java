68c3ee18be79305d01880a0965eec34928c04528
Vasi <https://github.com/VasiiH/am_i_rich>
2020-10-28 13:58:01 +0530

    Java char data type

---
+++ src/com/vasi/Main.java
@@ -0,0 +1,13 @@
+package com.vasi;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+
+        char mychar = '\u00d8';//unicode-table.com
+        System.out.println(mychar);
+
+    }
+}
